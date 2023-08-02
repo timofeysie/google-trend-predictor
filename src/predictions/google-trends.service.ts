95d70e90d57ac52c8a68e9722347dcdc1b64dcfd
tim_curchod <timofeyc@hotmail.com>
2023-08-02 09:43:19 +0900

    puppeteer working to get titles

--- src/predictions/google-trends.service.ts
+++ src/predictions/google-trends.service.ts
@@ -56,19 +56,26 @@ export class GoogleTrendsService {
   }
 
   async getSearchTrendsViePuppeteer(): Promise<string> {
-    const path_to_your_chrome_executable = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-    const browser = await puppeteer.launch({ headless: false, executablePath: path_to_your_chrome_executable });
+    const path_to_your_chrome_executable =
+      'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
+    const browser = await puppeteer.launch({
+      headless: false,
+      executablePath: path_to_your_chrome_executable,
+    });
+    console.log('puppeteer launched');
     try {
       const page = await browser.newPage();
+      console.log('puppeteer page ready');
       await page.setExtraHTTPHeaders({
         'accept-language': 'en-US,en;q=0.9',
       });
       const realTimeTrendsUrl = `https://trends.google.com/trends/trendingsearches/realtime?geo=${config.geo}&hl=${config.hl}&category=${config.category}`;
       await page.goto(realTimeTrendsUrl);
-
+      const trendingNowSelector = 'feed-item';
+      await page.waitForSelector(trendingNowSelector, { visible: true });
       const htmlContent = await page.content();
-      console.log('htmlContent', htmlContent.length);
-      // fs.writeFileSync('search_trends.html', htmlContent);
+      console.log('puppeteer htmlContent', htmlContent.length);
+      fs.writeFileSync('search_trends.html', htmlContent);
       return htmlContent;
     } catch (err) {
       console.error('Error while scraping:', err);
@@ -78,49 +85,29 @@ export class GoogleTrendsService {
     }
   }
 
-  private parseRealTimeTrendsWitCheerio(data: string): any
-  {
-
+  private parseRealTimeTrendsWitCheerio(data: string): any {
     const $ = cheerio.load(data);
     const html = $.html();
     console.log('HTML data:', html.length);
-    const body = html.indexOf("<body");
-    console.log('body', body)
-    console.log('html', html.substring(body, 100).length)
-
-    // Find the <md-list> element with class "md-list-block"
-    //const mdList = $('md-list.md-list-block');
-    //console.log('mdList', mdList.length) // 0
-
-    // Find all trend items within the <md-list> using the "feed-item-header" class
-    //const trendItems = mdList.find('div.feed-item-header');
-    //console.log('trendItems', trendItems.length) // 0
-
     const titles: string[] = [];
     const sparklineData: any[][] = [];
-
-    // Find all the elements with the class "title"
-    //const titleElements = $('div.title a');
-    //console.log('titleElements', titleElements.length) //0
-
     // Find the div with the specific attribute value containing the trending stories
-    const trendingStoriesDiv = $('feed-item');
-    console.log('trendingStoriesDiv', trendingStoriesDiv.length) // 0
+    const feedItemElements = $('feed-item');
+    console.log('feedItemElements', feedItemElements.length); // 0
     // Find all the "a" tags within the div
-    const trendElements = trendingStoriesDiv.find('a');
-    console.log('trendElements', trendElements.length)
+    const feedItemElementsAnchors = feedItemElements.find('a');
+    console.log('trendElements', feedItemElementsAnchors.length);
 
     // Loop through each trend element to extract title and sparkline data
-    trendElements.each((index, element) => {
+    feedItemElementsAnchors.each((index, element) => {
       const title = $(element).text().trim();
       titles.push(title);
 
       // Extract sparkline data from the "path" tag's "d" attribute
       const sparklinePath = $(element).siblings('path');
-      //const d = sparklinePath.attr('d');
       console.log('sparklinePath', sparklinePath.length);
       // const sparklinePoints = this.parseSparklineData(sparklinePath);
-      //sparklineData.push(sparklinePoints);
+      // sparklineData.push(sparklinePoints);
     });
 
     return { titles, sparklineData };
