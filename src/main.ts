331205cb89edd8e75132a4fd02ae86f8ea2088e1
tim_curchod <timofeyc@hotmail.com>
2024-12-30 23:33:43 +0900

    simplyfy cors config

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://dot-one-26b272efdbb8.herokuapp.com',
-      process.env.FRONTEND_URL,
-      process.env.DJANGO_API_URL,
-    ].filter(Boolean),
+    origin: 'https://dot-one-26b272efdbb8.herokuapp.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Content-Length', 'Content-Type'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204
+    credentials: false
   });
 
   await app.listen(3001);
