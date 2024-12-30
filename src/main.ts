d5a5c50c7f0940dbc53510165c23f796ca57de9f
tim_curchod <timofeyc@hotmail.com>
2024-12-30 09:42:37 +0900

    prepare app for ec2 use

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -7,6 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'http://ec2-52-65-222-223.ap-southeast-2.compute.amazonaws.com:3001',
       process.env.FRONTEND_URL,
       process.env.DJANGO_API_URL,
     ].filter(Boolean),
